e38e1610c538f1f72e4fd1f9001c99811a63a12b
Darwin Zhang <dwzhang@berkeley.edu>
2023-03-03 22:34:22 -0800

    basic working edge detector with multi-width

---
+++ src/main/scala/io_circuit/EdgeDetector.scala
@@ -0,0 +1,39 @@
+// See README.md for license details.
+
+package EdgeDetector
+
+import chisel3._
+import chisel3.util._
+
+/**
+ * A edge detector circuit that detects rising edges.
+ * It outputs a 1 cycle wide pulse when its corresponding input transitions from 0 to 1.
+ * @param WIDTH -> width of the data input/output
+ */
+
+class EdgeDetector(val WIDTH: Int) extends Module {
+  val io = IO(new Bundle {
+    /**
+     * IO consists of in/out signals
+     * The clock has been embedded into Chisel to achieve synchronization 
+     */
+    val data_in = Input(UInt(WIDTH.W))
+    val data_out = Output(Vec(WIDTH, UInt(1.W)))
+  })
+
+  // A reg that stores every bit of delayed input signal
+  val delay = RegInit(VecInit(Seq.fill(WIDTH)(0.U(1.W))))
+  // Check every !delayed signal input & signal input, creating one-clock-cycle pulse
+  
+  for (i <- 0 until WIDTH) {
+    delay(i) := RegNext(io.data_in(i))     // test for 2 cycle wide pulse
+    // delay(i) := io.data_in(i)
+    when(io.data_in(i) & !delay(i)) {
+      io.data_out(i) := 1.U
+    }
+    .otherwise {
+      io.data_out(i) := 0.U
+    }
+  }
+
+}
---
+++ src/test/scala/io_circuit/EDSpec.scala
@@ -0,0 +1,65 @@
+// See README.md for license details.
+
+package EdgeDetector
+
+import chisel3._
+import chiseltest._
+import org.scalatest.freespec.AnyFreeSpec
+import chisel3.experimental.BundleLiterals._
+
+
+class EDSpec extends AnyFreeSpec with ChiselScalatestTester {
+  "Edge Detector should detect rising edges and output a one-cycle wide pulse" in {
+
+    // test(new EdgeDetector(1)) { dut =>
+
+    //   // def readExpect(addr: Int, value: Int, port: Int = 0): Unit = {
+    //   //   dut.io.data_in(port).poke(addr.U)
+    //   //   dut.io.data_out(port).expect(value.U)
+    //   // }
+
+    //   // Initialize input and clock signals
+    //   dut.io.data_in.poke(0.U)
+    //   dut.clock.setTimeout(10)
+
+    //   // Wait for initial reset to complete
+    //   dut.clock.step(1)
+    
+    //   // Generate input pulse and wait for two clock cycles
+    //   dut.io.data_in.poke(1.U)
+    //   dut.clock.step(2)
+
+    //   // Check that output is synchronized to input
+    //   dut.io.data_out(0).expect(0.U)
+    //   dut.clock.step(1)
+
+    //   // Generate input pulse and wait for two clock cycles
+    //   dut.io.data_in.poke(1.U)
+    //   dut.clock.step(2)
+
+    //   // Check that output is only one cycle wide
+    //   dut.io.data_out(0).expect(0.U)
+
+    // }
+
+    
+    test(new EdgeDetector(2)) { c =>
+      c.io.data_in.poke(1.U)
+      c.clock.step(2)
+      c.io.data_out(0).expect(0.U)
+      c.io.data_out(1).expect(0.U)
+
+      c.io.data_in.poke(2.U)
+      c.clock.step(1)
+      c.io.data_out(0).expect(0.U)
+      c.io.data_out(1).expect(1.U)
+
+      c.clock.step(1)
+      c.io.data_out(0).expect(0.U)
+      c.io.data_out(1).expect(0.U)      
+
+
+    }
+  }
+
+}
