f5f824005a56c6dc29191ae0bf925be192846d2c
Darwin Zhang <dwzhang@berkeley.edu>
2023-05-21 18:53:52 -0700

    transmitter working & I'm back :))

--- src/main/scala/io_circuit/uart_transmitter.scala
+++ src/main/scala/io_circuit/uart_transmitter.scala
@@ -4,6 +4,7 @@ package uart_transmitter
 
 import chisel3._
 import chisel3.util._
+import chisel3.stage.ChiselStage
 
 /**
  * A uart transmitter circuit (a fifo_rx) that receives a 8-bit word from a producer block on FPGA via the ready-valid interface.
@@ -14,79 +15,77 @@ import chisel3.util._
  * SYMBOL_EDGE_TIME: width of a bit in cycles of the system clock -> CLOCK_FREQ / BAUD_RATE
  */
 
-class uart_transmitter(val CLOCK_FREQ: Int = 125000000, val BAUD_RATE: Int = 115200) extends Module {
+class uart_transmitter(val CLOCK_FREQ: Int = 1000, val BAUD_RATE: Int = 100) extends Module {
   val io = IO(new Bundle {
-
+    val reset = Input(Bool())
     val data_in = Input(UInt(8.W))
     val valid = Input(Bool())
     val ready = Output(Bool())
-    val serial_out = Output(UInt(1.W))
-
+    val serial_out = Output(Bool())
   })
 
-  val SYMBOL_EDGE_TIME = CLOCK_FREQ / BAUD_RATE
-  val SAMPLE_TIME = SYMBOL_EDGE_TIME / 2
-
+  // Internal signals
+  val cyclesPerSymbol = (CLOCK_FREQ.toDouble / BAUD_RATE.toDouble).toInt
   val symbol_edge = Wire(Bool())
   val start = Wire(Bool())
   val tx_running = Wire(Bool())
 
-  val tx_shift = RegInit(0.U(8.W))
-  val bit_cnt = Counter(11)
-  val clk_cnt = Counter(SYMBOL_EDGE_TIME)
+  // Counters & Shift register
+  val tx_shift = RegInit(0.U(10.W))
+  val bit_cnt = Counter(10)
+  val clkcyc_cnt = Counter(cyclesPerSymbol.toInt)
+
+  // Output the same bit for cyclesPerSymbol cycles
+  val tx_Reg = RegInit(1.B)
+
 
   //--|Signal Assignments|------------------------------------------------------
+  // symbol_edge -> when cycle counter hits the number of cycles of one symbol (bit) transmission
+  symbol_edge := clkcyc_cnt.value === (cyclesPerSymbol.U - 1.U)
 
-  // Goes high at every symbol edge
-  symbol_edge := clk_cnt.value === (SYMBOL_EDGE_TIME.U - 1.U)
-
-  // Goes high halfway through each symbol
-  // sample := clk_cnt.value === SAMPLE_TIME.U
-
-  // Goes high when it is time to start transmit a new character
+  // start -> when both valid signal from rx and ready signal agrees to transmit a 8-bit character
   start := io.valid && io.ready
 
-  // Goes high while transmitting a character
+  // tx_running -> bit counter counting indicates the transmitter is running (sending bits)
   tx_running := bit_cnt.value =/= 0.U
 
-  // When not transmitting, transmitter is ready to receive data
+  // io.ready -> When not sending bits, transmitter is ready to receive data
   io.ready := !tx_running
+
+  // io.serial_out -> The output signal has to be 1. the first bit of shift register; 2. within the entire cycle of symbol
+  io.serial_out := tx_shift(0) & tx_Reg
 
 
   //--|Counters|----------------------------------------------------------------
-
-  // Counts cycles until a single symbol is done
+  // clkcyc -> counts clock cycles until a single symbol is done
   when (start || io.reset || symbol_edge) {
-    clk_cnt.reset()
+    clkcyc_cnt.reset()
   }
   .otherwise {
-    clk_cnt.inc()
+    clkcyc_cnt.inc()
   }
 
-  // Counts down from 10 bits for every character
-  when (io.reset) {
-    bit_cnt.reset()
-  }
-  .elsewhen (start) {
+  // bit -> counts down 1 for every character whenever cycles-per-symbol is counted (1 bit is sent)
+  when (start) {
     bit_cnt.value := 10.U
   }
-  .elsewhen (symbol_edge && tx_running) {
+  .elsewhen (symbol_edge) {
     bit_cnt.value := bit_cnt.value - 1.U
   }
 
-
   //--|Shift Register|----------------------------------------------------------
+  when (io.reset) {
+    tx_shift := "b1111111111".U
+  }
   when (start) {
-    io.serial_out := 0.U
-    tx_shift := io.data_in
+    tx_shift := Cat(1.U, io.data_in, 0.U)
   }
   .elsewhen (symbol_edge) {
-    io.serial_out := tx_shift(0)
-    tx_shift := Cat(1.U, tx_shift(7, 1))
+    tx_shift := Cat(1.U, tx_shift(9, 1))
   }
-  .otherwise {
-    io.serial_out := 1.U
-  }
-
+  
 }
 
+object getVerilog extends App {
+  (new ChiselStage).emitVerilog(new uart_transmitter)
+}
--- src/test/scala/io_circuit/UATransmitSpec.scala
+++ src/test/scala/io_circuit/UATransmitSpec.scala
@@ -26,10 +26,11 @@ class UATransmitSpec extends AnyFreeSpec with ChiselScalatestTester {
       // check if transmitter is ready to receive data
       dut.io.ready.expect(true.B)
 
-      // transfer data into transmitter
+      // transfer data into transmitter, and check output status is going to send and not receiving anymore data
       dut.io.valid.poke(true.B)
       dut.io.data_in.poke(0xab.U)
       dut.clock.step(1)
+      dut.io.valid.poke(false.B)
       dut.io.ready.expect(false.B)
 
       // transmitter should begin sending the start bit
@@ -39,11 +40,113 @@ class UATransmitSpec extends AnyFreeSpec with ChiselScalatestTester {
       }
 
       // transmitter should send each data bit
-      for (bitIndex <- 0 until 8) {
-        for (i <- 0 until cyclesPerSymbol) {
-          dut.io.serial_out.expect((0xab >> bitIndex) & 0x1)
-          dut.clock.step(1)
-        }
+      // character 'B' (1011)
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(1.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(1.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(0.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(1.U)
+        dut.clock.step(1)
+      }
+
+      // character 'A' (1010)
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(0.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(1.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(0.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(1.U)
+        dut.clock.step(1)
+      }
+
+      // The computation does not work inside test case
+      // for (bitIndex <- 0 until 8) {
+      //   for (i <- 0 until cyclesPerSymbol) {
+      //     dut.io.serial_out.expect((0xab.U >> bitIndex) & 0x1.U)
+      //     dut.clock.step(1)
+      //   }
+      // }
+
+      // transmitter should begin sending the stop bit
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(1.U)
+        dut.clock.step(1)
+      }
+
+
+      // check if transmitter is ready to receive data
+      dut.io.serial_out.expect(1.U)
+      dut.io.ready.expect(true.B)
+      dut.clock.step(1)
+      dut.io.serial_out.expect(1.U)
+      dut.io.ready.expect(true.B)
+
+
+      // send another byte (0xcd)
+      dut.io.valid.poke(true.B)
+      dut.io.data_in.poke(0xcd.U)
+      dut.clock.step(1)
+      dut.io.valid.poke(false.B)
+      dut.io.ready.expect(false.B)
+
+      // transmitter should begin sending the start bit
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(0.U)
+        dut.clock.step(1)
+      }
+
+      // transmitter should send each data bit
+      // character 'D' (1101)
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(1.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(0.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(1.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(1.U)
+        dut.clock.step(1)
+      }
+
+      // character 'C' (1100)
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(0.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(0.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(1.U)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until cyclesPerSymbol) {
+        dut.io.serial_out.expect(1.U)
+        dut.clock.step(1)
       }
 
       // transmitter should begin sending the stop bit
@@ -52,33 +155,13 @@ class UATransmitSpec extends AnyFreeSpec with ChiselScalatestTester {
         dut.clock.step(1)
       }
 
-      // check if transmitter is ready to transmit
-      dut.io.serial_out.expect(1.U)
-      dut.io.ready.expect(true.B)
-      dut.clock.step(1)
-      dut.io.serial_out.expect(1.U)
-      dut.io.ready.expect(true.B)
 
-      /*
-      // start to transmit and check every bit
+      // check if transmitter is ready to receive data
       dut.io.serial_out.expect(1.U)
+      dut.io.ready.expect(true.B)
       dut.clock.step(1)
       dut.io.serial_out.expect(1.U)
-      dut.clock.step(1)
-      dut.io.serial_out.expect(1.U)
-      dut.clock.step(1)
-      dut.io.serial_out.expect(1.U)
-      dut.clock.step(1)
-      dut.io.serial_out.expect(1.U)
-      dut.clock.step(1)
-      dut.io.serial_out.expect(1.U)
-      dut.clock.step(1)
-      dut.io.serial_out.expect(1.U)
-      dut.clock.step(1)
-      dut.io.serial_out.expect(1.U)
-      dut.clock.step(1)
-      dut.io.serial_out.expect(1.U)
-      */
+      dut.io.ready.expect(true.B)
     }
   }
 }
